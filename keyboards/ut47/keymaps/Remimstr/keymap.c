cca94bbbdb7b9ad87ee8bfa9971ccfb4bb8a9ee7
Elizabeth Marchand <elizabethmarchand@Elizabeths-iMac.local>
2018-08-06 19:43:07 -0400

    Add Tab and Caps Lock

--- keyboards/ut47/keymaps/Remimstr/keymap.c
+++ keyboards/ut47/keymaps/Remimstr/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------------------+
    * | Down |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  | Spc |  UP  |
    * |-------------------------------------------------------------------------+
-   * |       |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  .  |  :  |  '  |Vol+ |
+   * | Tab   |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  .  |  :  |  '  |Vol+ |
    * |-------------------------------------------------------------------------+
    * |     |     | Fn  |Ctrl | Win |   Shift    | Alt  |     |     |     |Vol- |
    * `-------------------------------------------------------------------------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
   MO(2),   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SPC,   MO(2),
-  _______, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT,  KC_COLON, KC_QUOTE, KC_AUDIO_VOL_UP,
+  KC_TAB , KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT,  KC_COLON, KC_QUOTE, KC_AUDIO_VOL_UP,
   _______, _______, _______, KC_LCTRL, KC_LGUI,      MO(1),       KC_LALT, _______, _______,  _______,  KC_AUDIO_VOL_DOWN
 ),
 
@@ -63,10 +63,10 @@ LAYOUT(
    */
 
 LAYOUT(
-  RESET,  S(KC_Q),    S(KC_W),    S(KC_E),    S(KC_R),    S(KC_T),    S(KC_Y),   S(KC_U),    S(KC_I),    S(KC_O),  S(KC_P),   KC_DELETE,
-  _______,  S(KC_A),    S(KC_S),    S(KC_D),    S(KC_F),    S(KC_G),    S(KC_H),   S(KC_J),    S(KC_K),    S(KC_L),  KC_ENTER , _______,
-  _______,  S(KC_Z),    S(KC_X),    S(KC_C),    S(KC_V),    S(KC_B),    S(KC_N),   S(KC_M),    KC_COMMA,      KC_SCOLON,   KC_DOUBLE_QUOTE,    _______,
-  _______,  _______,       _______,       _______,       _______,                 _______,           _______,       _______,       _______,     _______,                  KC_AUDIO_MUTE
+  RESET,    S(KC_Q),    S(KC_W),    S(KC_E),    S(KC_R),    S(KC_T),    S(KC_Y),   S(KC_U),    S(KC_I),    S(KC_O),    S(KC_P),          KC_DELETE,
+  _______,  S(KC_A),    S(KC_S),    S(KC_D),    S(KC_F),    S(KC_G),    S(KC_H),   S(KC_J),    S(KC_K),    S(KC_L),    KC_ENTER,         _______,
+  KC_CAPS,  S(KC_Z),    S(KC_X),    S(KC_C),    S(KC_V),    S(KC_B),    S(KC_N),   S(KC_M),    KC_COMMA,   KC_SCOLON,  KC_DOUBLE_QUOTE,  _______,
+  _______,  _______,    _______,    _______,    _______,          _______,         _______,    _______,    _______,    _______,          KC_AUDIO_MUTE
 ),
 
   /* FN Layer 2 (UP)
