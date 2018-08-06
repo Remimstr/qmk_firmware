dd9eeecb58f662116990935add12093a9db3f626
Elizabeth Marchand <elizabethmarchand@Elizabeths-iMac.local>
2018-08-06 16:57:37 -0400

    Fix bug that caused stuck layers

--- keyboards/ut47/keymaps/Remimstr/keymap.c
+++ keyboards/ut47/keymaps/Remimstr/keymap.c
@@ -18,6 +18,7 @@
   #include "protocol/serial.h"
 #endif
 
+#define _______ KC_TRNS
 #define LT3_TAB LT(3, KC_TAB)
 #define MT_RSFT_ENT MT(MOD_RSFT, KC_ENT)
 
@@ -44,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
-  MO(3),   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SPC,   MO(2),
-  XXXXXXX, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT,  KC_COLON, KC_QUOTE, KC_AUDIO_VOL_UP,
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTRL, KC_LGUI,      MO(1),       KC_LALT, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_AUDIO_VOL_DOWN
+  MO(2),   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SPC,   MO(2),
+  _______, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT,  KC_COLON, KC_QUOTE, KC_AUDIO_VOL_UP,
+  _______, _______, _______, KC_LCTRL, KC_LGUI,      MO(1),       KC_LALT, _______, _______,  _______,  KC_AUDIO_VOL_DOWN
 ),
 
   /* FN Layer 1 (Shift)
    * ,-------------------------------------------------------------------------.
-   * |     |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  | Delete|
+   * |RESET|  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  | Delete|
    * |-------------------------------------------------------------------------+
    * |      |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  | Ret |      |
    * |-------------------------------------------------------------------------+
@@ -62,10 +63,10 @@ LAYOUT(
    */
 
 LAYOUT(
-  XXXXXXX,  LSFT(KC_Q),    LSFT(KC_W),    LSFT(KC_E),    LSFT(KC_R),    LSFT(KC_T),    LSFT(KC_Y),   LSFT(KC_U),    LSFT(KC_I),    LSFT(KC_O),  LSFT(KC_P),               KC_DELETE,
-  XXXXXXX,  LSFT(KC_A),    LSFT(KC_S),    LSFT(KC_D),    LSFT(KC_F),    LSFT(KC_G),    LSFT(KC_H),   LSFT(KC_J),    LSFT(KC_K),    LSFT(KC_L),  KC_ENTER ,                XXXXXXX,
-  XXXXXXX,  LSFT(KC_Z),    LSFT(KC_X),    LSFT(KC_C),    LSFT(KC_V),    LSFT(KC_B),    LSFT(KC_N),   LSFT(KC_M),    KC_COMMA,      KC_SCOLON,   LSFT(KC_DOUBLE_QUOTE),    XXXXXXX,
-  XXXXXXX,  XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,                 XXXXXXX,           XXXXXXX,       XXXXXXX,       XXXXXXX,     XXXXXXX,                  KC_AUDIO_MUTE
+  RESET,  S(KC_Q),    S(KC_W),    S(KC_E),    S(KC_R),    S(KC_T),    S(KC_Y),   S(KC_U),    S(KC_I),    S(KC_O),  S(KC_P),   KC_DELETE,
+  _______,  S(KC_A),    S(KC_S),    S(KC_D),    S(KC_F),    S(KC_G),    S(KC_H),   S(KC_J),    S(KC_K),    S(KC_L),  KC_ENTER , _______,
+  _______,  S(KC_Z),    S(KC_X),    S(KC_C),    S(KC_V),    S(KC_B),    S(KC_N),   S(KC_M),    KC_COMMA,      KC_SCOLON,   KC_DOUBLE_QUOTE,    _______,
+  _______,  _______,       _______,       _______,       _______,                 _______,           _______,       _______,       _______,     _______,                  KC_AUDIO_MUTE
 ),
 
   /* FN Layer 2 (UP)
@@ -81,10 +82,10 @@ LAYOUT(
    */
 
 LAYOUT(
-  XXXXXXX, KC_1,    KC_3,    KC_5,    KC_7,    KC_9,    XXXXXXX, KC_TILDE, KC_PLUS,  KC_ASTERISK, KC_EQUAL,   XXXXXXX,
-  XXXXXXX, KC_2,    KC_4,    KC_6,    KC_8,    KC_0,    XXXXXXX, XXXXXXX,  KC_MINUS, KC_SLASH,    KC_PERCENT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX
+  _______, KC_1,    KC_3,    KC_5,    KC_7,    KC_9,    _______, KC_TILDE, KC_PLUS,  KC_ASTERISK, KC_EQUAL,   _______,
+  _______, KC_2,    KC_4,    KC_6,    KC_8,    KC_0,    _______, _______,  KC_MINUS, KC_SLASH,    KC_PERCENT, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,     _______,    _______,
+  _______, _______, _______, _______, _______,      _______,     _______,  _______,  _______,     _______,    _______
 ),
 
   /* FN Layer 3 (DOWN)
@@ -100,11 +101,11 @@ LAYOUT(
    */
 
 LAYOUT(
-  XXXXXXX, KC_LEFT_PAREN,  KC_LEFT_CURLY_BRACE,  KC_LBRACKET, KC_LEFT_ANGLE_BRACKET,  XXXXXXX, XXXXXXX, KC_PIPE,   KC_EXCLAIM,  KC_AT,        KC_HASH,    XXXXXXX,
-  XXXXXXX, KC_RIGHT_PAREN, KC_RIGHT_CURLY_BRACE, KC_RBRACKET, KC_RIGHT_ANGLE_BRACKET, XXXXXXX, XXXXXXX, KC_BSLASH, KC_QUESTION, KC_AMPERSAND, KC_PERCENT, XXXXXXX,
-  XXXXXXX, XXXXXXX,        XXXXXXX,              XXXXXXX,     XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,     XXXXXXX,      XXXXXXX,    XXXXXXX,
-  XXXXXXX, XXXXXXX,        XXXXXXX,              XXXXXXX,              XXXXXXX,                XXXXXXX, XXXXXXX,   XXXXXXX,     XXXXXXX,      XXXXXXX,    XXXXXXX
-),
+  _______, KC_LEFT_PAREN,  KC_LEFT_CURLY_BRACE,  KC_LBRACKET, KC_LEFT_ANGLE_BRACKET,  _______, _______, KC_PIPE,   KC_EXCLAIM,  KC_AT,        KC_HASH,    _______,
+  _______, KC_RIGHT_PAREN, KC_RIGHT_CURLY_BRACE, KC_RBRACKET, KC_RIGHT_ANGLE_BRACKET, _______, _______, KC_BSLASH, KC_QUESTION, KC_AMPERSAND, KC_PERCENT, _______,
+  _______, _______,        _______,              _______,     _______,                _______, _______, _______,   _______,     _______,      _______,    _______,
+  _______, _______,        _______,              _______,              _______,                _______, _______,   _______,     _______,      _______,    _______
+)
 };
 
 //LED keymap functions
